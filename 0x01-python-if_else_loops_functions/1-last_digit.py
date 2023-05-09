d359906dbb5d6551548152a7019c9964a26ebee9
podegal <emmaomene@yahoo.com>
2023-05-09 16:38:52 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,11 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     last_digit = number % 10
 else:
-    last_digit = number % -10
-    print("Last digit of {} is {}".format(number, last_digit), end='')
-
+    last_digit = ((number * -1) % 10) * -1
+print(f"Last digit of {number:d} is {last_digit:d} and is", end=" ")
 if last_digit > 5:
-    print(" and is greater than 5")
+    print("greater than 5")
 elif last_digit == 0:
-    print(" and is 0")
+    print("0")
 else:
-    print(" and is less than 6 and not 0")
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for l in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(l)), end='')
+    print("{:c}".format(l), end="")
