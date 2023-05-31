072261704117821bb1076a91e248dc67ee14c107
podegal <emmaomene@yahoo.com>
2023-05-30 18:02:53 -0700

    a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    """Define a square with a size private
+    instance attribute"""
+
+    def __init__(self, size):
+        """Initializes __size of self with size"""
+        self.__size = size
